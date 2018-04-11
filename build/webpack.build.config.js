2e3d94b279b98a8f5f1b24ac6977d2251a3b9c5f
bichiKim <bichi@pjfactory.com>
2018-04-11 09:15:59 +0900

    update

--- build/webpack.build.config.js
+++ build/webpack.build.config.js
@@ -1,22 +1,11 @@
 const webpackBaseConfig = require('./webpack.base.config')
 const webpackMerge = require('webpack-merge')
+const externals = require('webpack-node-externals')
 module.exports = webpackMerge(webpackBaseConfig, {
   output: {
     libraryTarget: 'umd',
   },
   devtool: 'source-map',
   mode: 'production',
-  /**
-   * externals see
-   * @link https://www.npmjs.com/package/webpack-node-externals
-   */
-  externals: {
-    externals: {
-      lodash: {
-        commonjs: 'lodash',
-        amd: 'lodash',
-        root: '_',
-      },
-    },
-  },
+  externals: [externals()],
 })
